625d2e1ed6320814d86c329ab3da820531bf5385
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-04-27 23:16:39 -0400

    Moved all graphs to one file

--- server.r
+++ server.r
@@ -10,27 +10,6 @@ profiles <- read.csv("sample.csv")
 
 shinyServer(function(input, output) {
     
-    # Sivan's graphs
-    
-    output$wc_vs_sentiment <- renderPlot({
-        
-        ggplot(profiles) + 
-            geom_point(aes(x = polarity, y = wc)) + 
-            ggtitle("Word Count vs. Sentiment")
-        
-    })
-    
-    output$age_income_sentiment <- renderPlot({
-       
-        correlation <- cor(profiles$income, profiles$age, use = "na.or.complete") ** 2
-        
-        mytext <- paste("Age vs. Income with", "r^2 = ", correlation, ", colored by polarity")
-        
-        ggplot(subset(profiles, income < 250000 & income > 0)) + 
-            aes(x = age, y = income) + 
-            geom_point(aes(colour = polarity)) + 
-            scale_colour_gradientn(colours=c("#FF0000", "#ffffff", "#440088","#0000FF")) + 
-            geom_smooth(method = "lm") + 
-            ggtitle(mytext)
-    })
+    source("sivan.r")
+    render_sivans_graphs(output)
 })
---
+++ sivan.r
@@ -0,0 +1,35 @@
+render_sivans_graphs <- function(output)
+{
+
+    output$wc_vs_sentiment <- renderPlot({
+        
+        ggplot(profiles) + 
+            geom_point(aes(x = polarity, y = wc)) + 
+            ggtitle("Word Count vs. Sentiment")
+        
+    })
+    
+    output$age_income_sentiment <- renderPlot({
+        
+        correlation <- cor(profiles$income, profiles$age, use = "na.or.complete") ** 2
+        
+        mytext <- paste("Age vs. Income with", "r^2 = ", correlation, ", colored by Sentiment")
+        
+        ggplot(subset(profiles, income < 250000 & income > 0)) + 
+            aes(x = age, y = income) + 
+            geom_point(aes(colour = polarity)) + 
+            scale_colour_gradientn(colours=c("#FF0000", "#ffffff", "#440088","#0000FF")) + 
+            geom_smooth(method = "lm") + 
+            ggtitle(mytext)
+    })
+    
+    output$drinks_vs_words <- renderPlot({
+        ggplot(profiles) +
+            geom_point(aes(x = alcohol, 
+                           y = drinks, 
+                           size = polarity  * 1.5, 
+                           colour = polarity)) +
+            labs(x = "Number of Alcoholic Words", y = "Drinking Behavior") +
+            ggtitle("Number of Alcoholhic Words in Essays vs. Drinking Behavior")
+    })
+}
