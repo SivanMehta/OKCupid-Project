aa2403f5f01d30d7a8f32d194f57f5bfbd57b749
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-04-27 22:40:21 -0400

    add preprocess script to load in random sample of data

--- preprocess.r
+++ preprocess.r
@@ -6,7 +6,21 @@ library(qdap)
 profiles <- read.csv("profiles.csv")
 print("Loaded profiles.csv")
 
-# add alcohol word counts
+# add number of demands
+demands <- strsplit(as.character(profiles$essay9), "/. ")
+demands <- strsplit(as.character(demands), "and")
+demands <- strsplit(as.character(demands), ",")
+profiles$n_demands <- sapply(demands, length)
+print("Calculated number of demands")
+
+# add number of traits from user
+traits <- strsplit(as.character(profiles$essay0), "/. ")
+traits <- strsplit(as.character(traits), "and")
+traits <- strsplit(as.character(traits), ",")
+profiles$n_traits <- sapply(traits, length)
+print("Calculated number of traits")
+
+# add word counts
 essays <- select(profiles, starts_with("essay"))
 essays <- apply(essays, MARGIN=1, FUN=paste, collapse=" ")
 essays <- str_replace_all(essays, "\n", " ")
@@ -15,13 +29,15 @@ profiles$wine_count <- str_count(essays, "wine")
 profiles$beer_count <- str_count(essays, "beer")
 profiles$drink      <- str_count(essays, "drink")
 profiles$alcohol    <- profiles$wine_count + profiles$beer_count + profiles$drink
-print("made alcohol word counts")
+profiles$wc <-  sapply(strsplit(as.character(profiles$essay0), " "), length)
+
+print("Caclulated word counts")
 
 # calculate polarities
 sample <- profiles[sample(nrow(profiles), 2000), ]
 sample$polarity <- polarity(sample$essay0)$all$polarity
-print("made polarity calculation")
+print("Caclulated polarities")
 
 # write sample
 write.csv(sample, "sample.csv")
-print("wrote final file")
+print("Wrote final file")
