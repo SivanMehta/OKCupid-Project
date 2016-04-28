8ba1b08e842d122b7a6b6c6813828f5bc5d83b1d
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-04-28 11:59:46 -0400

    moved all unique plots into their own tabs

--- ui.r
+++ ui.r
@@ -6,8 +6,10 @@ sidebar <- dashboardSidebar( sidebarMenu(
         menuItem("Sentiment Analysis",    tabName = "sentiment",   icon = icon("chevron-right")),
         menuItem("Two Categorical Plots", tabName = "two_cats",    icon = icon("chevron-right")),
         menuItem("Word Clouds",           tabName = "clouds",      icon = icon("chevron-right")),
-        menuItem("Joyce",                 tabName = "Joyce",       icon = icon("chevron-right")),
-        menuItem("Suvrath",               tabName = "Suvrath",     icon = icon("chevron-right"))
+        menuItem("Livestyle vs. Age",     tabName = "life_v_age",  icon = icon("chevron-right")),
+        menuItem("Traits Offered vs. Demanded", tabName = "offered_v_demanded",  icon = icon("chevron-right")),
+        menuItem("Maps",               tabName = "maps",           icon = icon("chevron-right")),
+        menuItem("Mosiac Plot",        tabName = "mosaic",         icon = icon("chevron-right"))
     )
 )
 
@@ -37,42 +39,37 @@ body <- dashboardBody(
                         tabPanel("Fit",
                                  plotOutput(outputId = "wordCloud.fit", height = "600px")
                         ),
-                        
                         tabPanel("A little extra",
                                  plotOutput(outputId = "wordCloud.extra", height = "600px")
                         ),
-                        
                         tabPanel("Jacked",
                                  plotOutput(outputId = "wordCloud.jacked", height = "600px")
                         ),
-                        
                         tabPanel("Athletic",
                                  plotOutput(outputId = "wordCloud.athletic", height = "600px")
                         )
                     )
                 )
-    ),
-    tabItem(tabName = "Joyce",
-            mainPanel(
-                tabsetPanel(
-                    tabPanel("Lifestyle vs. Age",
-                             selectInput('x', 'X Variable', c("drinks", "diet", "body_type", "smokes")),
-                             plotOutput(outputId = "eda_age", height = "450px", width = "600px")
-                    ),
-                    tabPanel("Traits Offered vs. Traits Demanded",
-                             selectInput('color', 'Color According To', c("age", "drinks", "diet", "body_type", "smokes")),
-                             plotOutput(outputId = "offer_vs_demand", height = "450px", width = "600px"))
-                )
-            )
-    ),
-    tabItem(tabName = "Suvrath",
+        ),
+        tabItem(tabName = "life_v_age",
+            selectInput('x', 'X Variable', c("drinks", "diet", "body_type", "smokes")),
+            plotOutput(outputId = "eda_age", height = "450px", width = "600px")
+        ),
+        tabItem(tabName = "offered_v_demanded",
+            selectInput('color', 'Color According To', c("age", "drinks", "diet", "body_type", "smokes")),
+            plotOutput(outputId = "offer_vs_demand", height = "450px", width = "600px")
+        ),
+        tabItem(tabName = "maps",
             plotOutput(outputId = "map", height = "300px"),
             selectInput(inputId = "map_option",
                         label = h3("Select Variable"),
-                        choices = list("Smokes", "Drugs")),
+                        choices = list("Smokes", "Drugs"))
+        ),
+        tabItem(tabName = "mosaic",
             plotOutput(outputId = "mosaicplot", height = "300px")
+        )
     )
-))
+)
 
 # Put them together into a dashboardPage
 dashboardPage(
